66a0dc8ae324d69a81f5b4a3c01dbbddca0b6aa5
zhouyunhui <450442918@qq.com>
2017-04-05 09:50:12 +0800

    提交本地

--- src/components/store/CheckCard.vue
+++ src/components/store/CheckCard.vue
@@ -62,7 +62,7 @@ export default {
 </script>
 <style lang="less">
 .z-hori-menu {
-    margin-top: 0.16rem; //width:auto;
+    padding: 0.16rem 0; //width:auto;
     white-space: nowrap;
     .z-check-button {
         border-color: #C7C7C7;
--- src/components/store/Detail.vue
+++ src/components/store/Detail.vue
@@ -4,14 +4,14 @@
         <z-header :showBack="true"
                   backWords="">
             <span slot="rightitems">
-                                    <svg class="zicon zheader-icon zflex store-header-more" aria-hidden="true">
-                                        <use xlink:href="#real-icon-more"></use>
-                                    </svg>
-                                </span>
+                                        <svg class="zicon zheader-icon zflex store-header-more" aria-hidden="true">
+                                            <use xlink:href="#real-icon-more"></use>
+                                        </svg>
+                                    </span>
         </z-header>
         <!--头部结束-->
         <!--轮播开始-->
-        <div class="z-container">
+        <div class="z-container" id="z-container">
             <div class="z-store-carousel">
                 <tf-carousel :imgs="imgs"
                              :delay="4000"
@@ -24,34 +24,32 @@
                 <div class="z-storedetail-remind zflex">
                     <span>友情提示: </span>
                     <span class="remind-items zflex1">
-                                    <p> 1.维修需要时间，请耐心等待。</p>
-                                    <p> 2.自行送件到维修店，可免上门服务费哟。</p>
-                                </span>
+                                        <p> 1.维修需要时间，请耐心等待。</p>
+                                        <p> 2.自行送件到维修店，可免上门服务费哟。</p>
+                                    </span>
     
                 </div>
             </div>
             <!--友情提示结束-->
             <!--详情 简介 评价-->
-            <div class="z-box z-store-detail"
-                 style="height:90px;">
-                <sticky scroll-box="z-container"
-                        :offset="46"
-                        :check-sticky-support="false">
-                    <tab :line-width="1"
-                         active-color="#f9261d"
-                         default-color="#969696"
-                         bar-active-color="#f9261d">
-                        <tab-item selected>服务项目</tab-item>
-                        <tab-item>商家简介</tab-item>
-                        <tab-item>评价(135)</tab-item>
-                    </tab>
-                    <!--<check-card :items="items"></check-card>-->
-                </sticky>
+            <sticky class="stickbox" scroll-box="z-container">
+                <tab :line-width="1"
+                     active-color="#f9261d"
+                     default-color="#969696"
+                     bar-active-color="#f9261d">
+                    <tab-item selected>服务项目</tab-item>
+                    <tab-item>商家简介</tab-item>
+                    <tab-item>评价(135)</tab-item>
+                </tab>
+               
+            </sticky>
+            <div class="z-box z-store-detail">
+                 <check-card :items="items"></check-card>
+                <service-item container=".z-store-detail"
+                              v-for="item in serviceItems"
+                              :item="item"
+                              :key="item"></service-item>
             </div>
-            <service-item container=".z-store-detail"
-                          v-for="item in serviceItems"
-                          :item="item"
-                          :key="item"></service-item>
         </div>
         <!--详情 简介 评价-->
     </div>
@@ -66,17 +64,22 @@ import CheckCard from './CheckCard'
 import Tab from 'vux/src/components/tab/tab.vue'
 import TabItem from 'vux/src/components/tab/tab-item.vue'
 import ServiceItem from './ServiceItem.vue'
+//import Sticky from '@/components/common/Sticky'
 export default {
     name: 'storeDetail',
     components: {
         ZHeader,
         TfCarousel,
-        Sticky,
+        // Sticky,
         CheckCard,
         Tab,
         TabItem,
-        ServiceItem
+        ServiceItem,
+        Sticky
     },
+    // directives: {
+    //     'sticky': Sticky
+    // },
     data() {
         return {
             imgs: [],
@@ -90,6 +93,10 @@ export default {
                 '修理各种疑难杂症'
             ],
             serviceItems: [
+                { id: 1, src: require('../../assets/imgs/test.png'), title: '滚筒洗衣机上门维修(检测费)', desc: '服务内容：1.总费用=一口价+特殊配置特殊配置特殊配置特殊配置', price: 49, unit: '项', sale: 418 },
+                { id: 1, src: require('../../assets/imgs/test.png'), title: '滚筒洗衣机上门维修(检测费)', desc: '服务内容：1.总费用=一口价+特殊配置特殊配置特殊配置特殊配置', price: 49, unit: '项', sale: 418 },
+                { id: 1, src: require('../../assets/imgs/test.png'), title: '滚筒洗衣机上门维修(检测费)', desc: '服务内容：1.总费用=一口价+特殊配置特殊配置特殊配置特殊配置', price: 49, unit: '项', sale: 418 },
+                { id: 1, src: require('../../assets/imgs/test.png'), title: '滚筒洗衣机上门维修(检测费)', desc: '服务内容：1.总费用=一口价+特殊配置特殊配置特殊配置特殊配置', price: 49, unit: '项', sale: 418 },
                 { id: 1, src: require('../../assets/imgs/test.png'), title: '滚筒洗衣机上门维修(检测费)', desc: '服务内容：1.总费用=一口价+特殊配置特殊配置特殊配置特殊配置', price: 49, unit: '项', sale: 418 },
                 { id: 1, src: require('../../assets/imgs/test.png'), title: '滚筒洗衣机上门维修(检测费)', desc: '服务内容：1.总费用=一口价+特殊配置特殊配置特殊配置特殊配置', price: 49, unit: '项', sale: 418 },
                 { id: 1, src: require('../../assets/imgs/test.png'), title: '滚筒洗衣机上门维修(检测费)', desc: '服务内容：1.总费用=一口价+特殊配置特殊配置特殊配置特殊配置', price: 49, unit: '项', sale: 418 },
@@ -124,5 +131,10 @@ export default {
 
 .z-storedetail-remind.zflex {
     align-items: flex-start
+}
+
+.stickbox {
+    overflow: hidden;
+    background-color: #fff;
 }
 </style>
